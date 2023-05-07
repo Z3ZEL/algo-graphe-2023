0b133c5eaa525ddba062818aa72a92ff135adbd3
Z3ZEL <estebanrodriguez17780@gmail.com>
2023-05-07 12:24:47 +0200

    dijstrak v2

--- dijkstra.py
+++ dijkstra.py
@@ -1,4 +1,5 @@
 from graph_drone import Graph_drone
+import math as m;
 
 class Dijkstra:
     def __init__(self, graph):
@@ -7,57 +8,48 @@ class Dijkstra:
 
     def get_adjacent_vertices(self, v):
         adjacent_vertices = []
-        for i in range(self.graph.num_vertices):
-            if self.graph.adj_matrix[v][i] > 0:
+        for i in range(len(self.graph.adj_matrix[v])):
+            if self.graph.adj_matrix[v][i] != 0:
                 adjacent_vertices.append(i)
         return adjacent_vertices
-
-    def get_next_vertex(self, distance, visited, start_index):
-        vertices = self.get_adjacent_vertices(start_index)
-        min_distance = -1
+    def get_next_vertex(self,Q, distance):
+        min = m.inf
         min_index = -1
-        for i in vertices:
-            if not visited[i] and (min_distance == -1 or distance[i] < min_distance):
-                min_distance = distance[i]
+        for i in range(len(Q)):
+            if distance[Q[i]] < min:
+                min = distance[Q[i]]
                 min_index = i
-        return min_index
+        return Q[min_index]
+        
 
 
 
 
     def caculate(self, start_pos):
-        #init map at -1
-        map = [[-1 for _ in range(self.graph.num_vertices)] for _ in range(self.graph.num_vertices)]
-        #init distance at -1
-        distance = [-1 for _ in range(self.graph.num_vertices)]
+        #init distance at inf
+        distance = [m.inf for _ in range(self.graph.num_vertices)]
         #init visited at False
-        visited = [False for _ in range(self.graph.num_vertices)]
+        Q = [ i for i in range(self.graph.num_vertices)]
 
         #init start position
 
         start_index = self.graph.pos_to_index(start_pos[0], start_pos[1])
-        
         distance[start_index] = 0  #set distance to 0
-        visited[start_index] = True  #set visited to True
-
-
-
-        #set map to start index
-        map[start_pos[0]][start_pos[1]] = 0 #DISTANCE FROM START TO START IS 0
-
         current = start_index
-        while current != -1:
-            adjacent_vertices = self.get_adjacent_vertices(current)
-            for i in adjacent_vertices:
-                if not visited[i]:
-                    if distance[i] == -1 or distance[i] > distance[current] + self.graph.adj_matrix[current][i]:
-                        distance[i] = distance[current] + self.graph.adj_matrix[current][i]
-                    
-            visited[current] = True
-            current = self.get_next_vertex(distance, visited, current)
-        for i in range(self.graph.num_vertices):
-            (x,y) = self.graph.index_to_pos(i)
-            map[x][y] = distance[i]
+
+        #DIJKSTRA ALGORITHM
+        while len(Q) != 0:
+            #get adjacent vertices
+            current = self.get_next_vertex(Q, distance)
+            Q.remove(current)
+            #update distance
+            for i in self.get_adjacent_vertices(current):
+                if distance[i] > distance[current] + self.graph.adj_matrix[current][i]:
+                    distance[i] = distance[current] + self.graph.adj_matrix[current][i]
+        #replace inf by -1
+        for i in range(len(distance)):
+            if distance[i] == m.inf:
+                distance[i] = -1
         return distance
 
 
--- graph_test.py
+++ graph_test.py
@@ -1,7 +1,12 @@
 from graph_encoding import decode_graph_file
+import dijkstra as dj
 
 graph = decode_graph_file(10, 'world.txt')
 graph.print_data()
 graph.print_world()
 graph.print_vilage_constrains()
+
+dijkstra = dj.Dijkstra(graph)
+print(dijkstra.caculate((0,0)))
+
 # graph.print_adj_matrix()
