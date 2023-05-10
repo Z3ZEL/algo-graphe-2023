9f7314db0a56dc527cddd320bcfcd4d53c71ae7e
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2023-05-10 14:50:33 +0200

    mise au propre

--- graph_part_3.py
+++ graph_part_3.py
@@ -5,6 +5,79 @@ from math import sqrt
 import subprocess
 import random
 
+def create_dot_file(adj_matrix, filename):
+    with open(filename, 'w') as file:
+        # Write the header of the DOT file
+        file.write("graph {\n")
+        # Loop through all the vertices
+        for i in range(len(adj_matrix)):
+            # Loop through all the adjacent vertices
+            for j in range(i, len(adj_matrix[i])):
+                # If there is an edge between i and j, add it to the DOT file
+                if adj_matrix[i][j] > 0 and i != j:
+                    file.write(f"    {i+1} -- {j+1} [label=\"{adj_matrix[i][j]}\"];\n")
+        # Write the footer of the DOT file
+        file.write("}")
+        subprocess.run([f'circo -Tpng {filename} -o {filename}.png'], shell=True)
+
+def closest_drone_to(drones, village ,village_graph):
+    min = 1000000000
+    index = -1
+    for drone in drones:
+        if village_graph[village][drone] <= min:
+            min = village_graph[village][drone]
+            index = drone
+    return(min,index)
+
+def closest_village_to(villages, drone ,village_graph):
+    min = 1000000000
+    index = -1
+    for village in villages:
+        if village_graph[drone][village] <= min:
+            min = village_graph[drone][village]
+            index = village
+    return(min,index)
+
+#First idea : for each village get the closest drone 
+def village_order_1(graph,village_graph):                                            
+    t_loss = 0
+    drones = list(map(graph.drone_to_village_index,graph.drones))
+    already_seen = drones
+    villages = [ i for i in range(len(graph.villages)) ]
+    while( len(already_seen) != len(villages)):
+        for village in villages :   
+            if village not in already_seen: 
+                (t_min,index_drone) = closest_drone_to(drones,village,village_graph)        #get the idnex of the closest drone to the village
+                print("le village :", village," appelle le drone du village :" ,index_drone," et met  :",t_min," minutes pour s'y rendre.")
+                t_loss+=t_min                                                             
+                drones[index_drone] = village       #move the drone to its new location 
+                already_seen.append(village)
+
+    return t_loss
+
+#Second idea : for each drone move it to the closest village 
+def village_order_2(graph,village_graph):
+    t_loss = 0
+    drones = list(map(graph.drone_to_village_index,graph.drones))
+    already_seen = drones.copy()
+    villages = []
+    for i in range(len(village_graph)):
+        if i not in already_seen:
+            villages.append(i)
+    while(len(villages) > 0):
+        t_tot = []  
+        for drone_index in range(len(drones)) :
+            if(len(villages) ==0):
+                break
+            (t_min,village_index) = closest_village_to(villages,drones[drone_index],village_graph)
+            print("le drone du village :", drones[drone_index]," se déplace vers :" ,village_index," en :",t_min," minutes.")
+            drones[drone_index] = village_index                                               #move the drone to its new location 
+            already_seen.append(village_index)
+            villages.remove(village_index)
+            t_tot.append(t_min)
+        t_loss += max(t_tot)
+    return t_loss
+#### --------- MAIN ---------
 ###--------PARAMETERS--------
 
 N = 100 #NODE NUMBER
@@ -51,81 +124,8 @@ for i in range(len(village_graph)):
         print(village_graph[i][j], end=' ')
     print()
 
-def create_dot_file(adj_matrix, filename):
-    with open(filename, 'w') as file:
-        # Write the header of the DOT file
-        file.write("graph {\n")
-        # Loop through all the vertices
-        for i in range(len(adj_matrix)):
-            # Loop through all the adjacent vertices
-            for j in range(i, len(adj_matrix[i])):
-                # If there is an edge between i and j, add it to the DOT file
-                if adj_matrix[i][j] > 0 and i != j:
-                    file.write(f"    {i+1} -- {j+1} [label=\"{adj_matrix[i][j]}\"];\n")
-        # Write the footer of the DOT file
-        file.write("}")
-        subprocess.run([f'circo -Tpng {filename} -o {filename}.png'], shell=True)
-
 create_dot_file(village_graph, 'village_graph.dot')
 
-def closest_drone_to(drones, village ,village_graph):
-    min = 1000000000
-    index = -1
-    for drone in drones:
-        if village_graph[village][drone] <= min:
-            min = village_graph[village][drone]
-            index = drone
-    return(min,index)
-
-def closest_village_to(villages, drone ,village_graph):
-    min = 1000000000
-    index = -1
-    for village in villages:
-        if village_graph[drone][village] <= min:
-            min = village_graph[drone][village]
-            index = village
-    return(min,index)
-
-#First idea : for each village get the closest drone 
-def village_order_1(graph,village_graph):                                            
-    t_loss = 0
-    drones = list(map(graph.drone_to_village_index,graph.drones))
-    already_seen = drones
-    villages = [ i for i in range(len(graph.villages)) ]
-    while( len(already_seen) != len(villages)):
-        for village in villages :   
-            if village not in already_seen: 
-                (t_min,index_drone) = closest_drone_to(drones,village,village_graph)        #get the idnex of the closest drone to the village
-                print("le village :", village," appelle le drone du village :" ,index_drone," et met  :",t_min," minutes pour s'y rendre.")
-                t_loss+=t_min                                                             
-                drones[index_drone] = village       #move the drone to its new location 
-                already_seen.append(village)
-
-    return t_loss
-
 print("Temps total pour les fuites : ",village_order_1(graph,village_graph))
-
-#Second idea : for each drone move it to the closest village 
-def village_order_2(graph,village_graph):
-    t_loss = 0
-    drones = list(map(graph.drone_to_village_index,graph.drones))
-    already_seen = drones.copy()
-    villages = []
-    for i in range(len(village_graph)):
-        if i not in already_seen:
-            villages.append(i)
-    while(len(villages) > 0):
-        t_tot = []  
-        for drone_index in range(len(drones)) :
-            if(len(villages) ==0):
-                break
-            (t_min,village_index) = closest_village_to(villages,drones[drone_index],village_graph)
-            print("le drone du village :", drones[drone_index]," se déplace vers :" ,village_index," en :",t_min," minutes.")
-            drones[drone_index] = village_index                                               #move the drone to its new location 
-            already_seen.append(village_index)
-            villages.remove(village_index)
-            t_tot.append(t_min)
-        t_loss += max(t_tot)
-    return t_loss
 
 print("Temps total pour les fuites : ", village_order_2(graph,village_graph))
